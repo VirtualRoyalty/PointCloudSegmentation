d2eedaa73b90fde8250c307de31906428f782561
iamilyasedunov <iliasedunov@yandex.ru>
2020-03-24 16:55:52 +0300

    big new structure commit

---
+++ obstacle-detection/pipeline/common.py
@@ -0,0 +1,124 @@
+"""
+Our implementation of obstacle detection pipeline steps
+@authors:
+"""
+import numpy as np
+import pandas as pd
+
+
+
+def roi_filter(pcloud, verbose=True, **params):
+    """
+
+    Region Of Interest function, which filter required area
+    that relative to LIDAR scanner (point (0, 0, 0) is a center)
+
+    """
+    if verbose:
+        print('Input pcloud size: {}'.format(len(pcloud)))
+    pcloud['camera'] = ((pcloud['x'] > params['min_x']) & (pcloud['x'] < params['max_x']) &
+                       (pcloud['y'] >  params['min_y']) & (pcloud['y'] < params['max_y']) &
+                       (pcloud['z'] >  params['min_z']) & (pcloud['z'] <  params['max_z']))
+    pcloud = pcloud[pcloud['camera'] == True]
+    if verbose:
+        print('Output ROI pcloud size: {}'.format(len(pcloud)))
+    return pcloud
+
+
+def obstacle_filter(pcloud, obstacle_lst, proc_labels=True, verbose=True):
+    """
+
+    Obstacle filtering function
+    pcloud: pandas.DataFrame,
+    Point cloud DataFrame that have columns=['x', 'y', 'z', 'seg_id']
+
+    obstacle_lst: list,
+    A list of segments id you want to be remain after filtering
+    """
+    # sanity check
+    assert isinstance(pcloud, pd.DataFrame)
+
+    origin_point_size = len(pcloud)
+
+    if proc_labels:
+        pcloud.seg_id = pcloud.seg_id.astype('uint32')
+        pcloud.seg_id = pcloud.seg_id.apply(lambda x: x & 0xFFFF)
+
+    pcloud = pcloud[pcloud['seg_id'].isin(list(obstacle_lst.keys()))]
+    if verbose:
+        print('Filter required segments')
+        print('Point size before: {} and after filtering: {}'.format(origin_point_size, len(pcloud)))
+
+    return pcloud
+
+
+def outlier_filter(tcluster):
+
+    # tcluster['norm'] = np.sqrt(np.square(tcluster).sum(axis=1))
+    try:
+        _mean, _std = tcluster['norm'].mean(), tcluster['norm'].std()
+        lower, higher = _mean - 3 * _std, _mean + 3 * _std
+    except:
+        tcluster['norm'] = np.sqrt(np.square(tcluster[['x', 'y', 'z']]).sum(axis=1))
+        _mean, _std = tcluster['norm'].mean(), tcluster['norm'].std()
+        lower, higher = _mean - 3 * _std, _mean + 3 * _std
+
+    # tcluster['outlier'] = ((tcluster['norm'] < lower) |
+    #                        (tcluster['norm'] > higher))
+    tcluster['outlier'] = tcluster['norm'].apply(lambda x: True if x < lower or x > higher else False)
+
+    tcluster = tcluster[~tcluster.outlier]
+
+    return tcluster
+
+
+
+def get_bounding_boxes(clusters):
+    box_coord_list = []
+    for i in range(len(clusters)):
+        x_min, x_max, y_min, y_max, z_min, z_max =  list(clusters.iloc[i])
+        box = np.zeros([8, 3])
+        box[0, :] = [x_min, y_min, z_min]
+        box[1, :] = [x_max, y_min, z_min]
+        box[2, :] = [x_max, y_max, z_min]
+        box[3, :] = [x_min, y_max, z_min]
+        box[4, :] = [x_min, y_min, z_max]
+        box[5, :] = [x_max, y_min, z_max]
+        box[6, :] = [x_max, y_max, z_max]
+        box[7, :] = [x_min, y_max, z_max]
+        box = np.transpose(box)
+        box_coord_list.append(box)
+    return box_coord_list
+
+
+def get_optimal_bboxes(clusters, cluster_data):
+    box_coord_list = []
+    cov_matrix = np.cov(cluster_data[['x', 'y', 'z']].T, bias=True)
+    eig = np.linalg.eig(cov_matrix)
+    clusters = []
+    for cl_id in cluster_data.cluster_id.unique():
+        cluster = cluster_data[cluster_data.cluster_id == cl_id]
+        cluster = np.dot(cluster[['x', 'y', 'z']], eig[1])
+        cluster = pd.DataFrame(cluster, columns = ['x', 'y', 'z'])
+        cluster = cluster.agg({ 'x':['min','max'],
+                                'y':['min','max'],
+                                'z':['min','max']
+                                  })
+        clusters.append(cluster.T)
+    for i in range(len(clusters)):
+        x_min, x_max =  list(clusters[i].values[0])
+        y_min, y_max =  list(clusters[i].values[1])
+        z_min, z_max =  list(clusters[i].values[2])
+        box = np.zeros([8, 3])
+        box[0, :] = [x_min, y_min, z_min]
+        box[1, :] = [x_max, y_min, z_min]
+        box[2, :] = [x_max, y_max, z_min]
+        box[3, :] = [x_min, y_max, z_min]
+        box[4, :] = [x_min, y_min, z_max]
+        box[5, :] = [x_max, y_min, z_max]
+        box[6, :] = [x_max, y_max, z_max]
+        box[7, :] = [x_min, y_max, z_max]
+
+        box = np.transpose(box)
+        box_coord_list.append(box)
+    return box_coord_list
---
+++ obstacle-detection/scripts/test.py
@@ -0,0 +1,126 @@
+import re
+import time
+import json
+import numpy as np
+
+from datetime import datetime
+from tqdm import tqdm, tqdm_notebook
+
+from sklearn.model_selection import ParameterGrid
+
+
+def grid_search_optimization(scan, label, obstacle_lst, pipeline, params, verbose=True):
+    """
+    Grid Search of hyperparametrs for optimization of executional time of pipeline
+
+    scan: numpy.array,
+    An N X 3 array of point cloud from LIDAR
+
+    label: numpy.array,
+    A list of segmentation labels respectively
+
+    obstacle_lst: list,
+    A list of obstacles id
+
+    pipeline: function,
+    An obstacle-detection pipeline function
+
+    params: dict,
+    A dictionary of params range that is reqired to search
+
+    verbose: bool, optional, defualt True
+    Wheter or not print info during execution.
+
+    """
+    time_exec_lst = {}
+    for param in ParameterGrid(params):
+        start_time = datetime.now()
+        clusters, _, exec_time =  pipeline(scan, label, obstacle_lst,exec_time=True, verbose=False, **param)
+        end_time = sum(exec_time.values())
+        print('Total time {} ms. Created {} clusters'.format(end_time, len(clusters)))
+        if verbose:
+            print('*' * 40)
+            print(json.dumps(param, indent=3))
+            print(json.dumps(exec_time, indent=3))
+            print('*' * 40)
+            print()
+        time_exec_lst[json.dumps(param, indent=3)] = (end_time, len(clusters))
+    return time_exec_lst
+
+
+def get_scan_id(scan):
+    return re.findall(r'\d\d\d\d\d.', scan)[0]
+
+
+def get_bbox_and_stat(scan_lst, labels_lst, obstacle_lst, pipeline,
+                      write_path=None, detailed=False, **pipeline_params):
+    """
+    Gettitng bounding boxes for reqired sequence of scans and labels
+    Also ability to grep time execution statistic.
+
+    scan_lst: list,
+    A list of LIDAR scans
+
+    labels_lst: list,
+    A list of labels respectively
+
+    obstacle_lst: list,
+    A list of obstacles id
+
+    pipeline: function,
+    An obstacle-detection pipeline function with required args
+
+    write_path: string, optional, default None
+    A path where to write labels. If None labels will not be recorded
+
+    detailed: bool, optional, default False
+    If True there will be time execution statistic returned
+
+    """
+
+    # sanity check
+    assert len(scan_lst) == len(labels_lst)
+
+    exec_time_dct = {}
+    clusters_minmax_dct = {}
+    stats = []
+
+    try:
+        for scan, label in tqdm_notebook(zip(sorted(scan_lst), sorted(labels_lst)),
+                            total=len(scan_lst), desc='Scan processed'):
+            # sanity check
+            scan_id = get_scan_id(scan)
+            assert scan_id == get_scan_id(label)
+
+            # read scan
+            scan = np.fromfile(scan, dtype=np.float32)
+            scan = scan.reshape((-1, 4))[:, :3]
+
+            # read label
+            label = np.fromfile(label, dtype=np.uint32)
+            label = label.reshape((-1))
+
+            start_time = datetime.now()
+            # start pipeline
+            if detailed:
+                clusters, _, stat = pipeline(scan, label, obstacle_lst, exec_time=True, **pipeline_params)
+                stats.append(stat)
+            else:
+                clusters, _ = pipeline(scan, label, obstacle_lst, **pipeline_params)
+
+            end_time =  datetime.now() - start_time
+            exec_time_dct[str(scan_id)[-3:]] = end_time.microseconds / 1000
+            clusters_minmax_dct[str(scan_id)[-3:]] = clusters
+
+            if write_path:
+                # write cluster in format x_min, x_max, y_min, y_max, z_min, z_max
+                clusters_lst = []
+                for segment in clusters:
+                    for cluster in segment:
+                        clusters_lst.append(cluster)
+                np.savetxt(write_path + str(scan_id) + '.bbox', clusters_lst)
+    except KeyboardInterrupt:
+        print('User`s KeyboardInterruption...')
+        return clusters_minmax_dct, exec_time_dct, stats
+
+    return clusters_minmax_dct, exec_time_dct, stats
