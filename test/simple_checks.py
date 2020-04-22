0fb5dfc505b55c8caa4761e62beda91d4caba9ff
Vadik <vadikalperovich@mail.ru>
2020-04-23 00:07:49 +0300

    tests for common.py implemented

--- test/simple_checks.py
+++ test/simple_checks.py
@@ -1,4 +1,3 @@
-
 import sys
 import yaml
 import glob
@@ -11,7 +10,7 @@ sys.path.append('./obstacle-detection/')
 from pipeline import common
 
 
-scan_lst   = sorted(glob.glob("./test/data/*.bin"))
+scan_lst = sorted(glob.glob("./test/data/*.bin"))
 label_lst = sorted(glob.glob("./test/data/*.label"))
 
 
@@ -21,10 +20,10 @@ obstacle_lst = config['obstacles']
 
 
 def get_pcloud(scan, label, proc_label=True):
-    scan   = np.fromfile(scan, dtype=np.float32)
-    scan   = scan.reshape((-1, 4))
-    label  = np.fromfile(label, dtype=np.uint32)
-    label  = label.reshape((-1))
+    scan = np.fromfile(scan, dtype=np.float32)
+    scan = scan.reshape((-1, 4))
+    label = np.fromfile(label, dtype=np.uint32)
+    label = label.reshape((-1))
     values = np.concatenate((scan, label.reshape(len(label), 1)), axis=1)
     pcloud = pd.DataFrame(values, columns=['x', 'y', 'z', 'seg_id'])
     if proc_labels:
@@ -35,43 +34,39 @@ def get_pcloud(scan, label, proc_label=True):
 
 class TestClass:
 
-
     def test_obstacle_filter_1(self):
         pcloud = get_pcloud(scan_lst[0], label_lst[0])
-        cloud  = common.obstacle_filter(pcloud,
+        cloud = common.obstacle_filter(pcloud,
                                         obstacle_lst,
                                         proc_label = False)
         seg_lst = list(cloud['seg_id'].unique())
         for seg in seq_lst:
             assert seg in list(obstacle_lst.keys())
 
-
     def test_obstacle_filter_2(self):
         pcloud = get_pcloud(scan_lst[1], label_lst[1])
-        cloud  = common.obstacle_filter(pcloud,
+        cloud = common.obstacle_filter(pcloud,
                                         obstacle_lst,
                                         proc_label=False)
         seg_lst = list(cloud['seg_id'].unique())
         for seg in seq_lst:
             assert seg in list(obstacle_lst.keys())
-
 
     def test_obstacle_filter_3(self):
         pcloud = get_pcloud(scan_lst[2], label_lst[2])
-        cloud  = common.obstacle_filter(pcloud,
+        cloud = common.obstacle_filter(pcloud,
                                         obstacle_lst,
                                         proc_label=False)
         seg_lst = list(cloud['seg_id'].unique())
         for seg in seq_lst:
             assert seg in list(obstacle_lst.keys())
-
 
     def test_roi_filter_1(self):
         params = {'roi_x_min': -10, 'roi_x_max': 10,
                   'roi_y_min': -14, 'roi_y_max': 14,
-                  'roi_z_min': -2,  'roi_z_max': 1}
+                  'roi_z_min': -2, 'roi_z_max': 1}
         pcloud = get_pcloud(scan_lst[0], label_lst[0])
-        cloud  = common.roi_filter( pcloud,
+        cloud = common.roi_filter(pcloud,
                                     min_x=params["roi_x_min"],
                                     max_x=params["roi_x_max"],
                                     min_y=params["roi_y_min"],
